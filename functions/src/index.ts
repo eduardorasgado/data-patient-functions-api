9d52dc199129e693f211ca6fd5096061ee946685
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-06-09 10:43:53 -0500

    All the rest of microservices in different repos has been moved into this.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,6 +19,9 @@ const app = express();
 const main = express();
 
 const pacientesBasicDataCollection = 'pacientesBasicData';
+const personalDataCollection = 'personalDataCollection';
+const pacientesAlAnDataCollection = 'pacientesAlergiasyAntecedentesData';
+const publicDataCollection = 'patientPublicData';
 
 main.use('/api/v1', app);
 main.use(bodyParser.json());
@@ -161,3 +164,354 @@ app.delete('/paciente/basic/:patientId', (req, res) => {
             }
         })
 });
+
+
+/**
+ * ============== RUTAS PARA TRABAJAR CON DATOS PERSONALES DEL PACIENTE
+ */
+
+ /**
+  * Metodo para insertar datos personales de un paciente
+  */
+ app.post('/paciente/personal', (req, res) => {
+    const body = req.body;
+    if(body != null) {
+        firebaseHelper.firestore
+           .createDocumentWithID(
+               db,
+               personalDataCollection,
+               body.paciente_id,
+               {
+                   "telefono": body.telefono,
+                   "tipo_de_sangre": body.tipo_de_sangre,
+                   "fuma": body.fuma,
+                   "drogas": body.drogas,
+                   "bebidas_alcoholicas": body.bebidas_alcoholicas,
+                   // TODO: proteger este dato con criptografia
+                   "num_seguro_social": body.num_seguro_social,
+                   "seguro_medico": body.seguro_medico,
+                   "internado_hospitalario": body.internado_hospitalario,
+                   "cirugia": body.cirugia,
+                   "actividad_fisica": body.actividad_fisica,
+                   "antecedentes_enfermedades_importantes": body.antecedentes_enfermedades_importantes
+               }
+           )
+           .then((response:any) => {
+               res
+                   .status(201)
+                   .send({
+                       status: 'success',
+                       message: 'Datos personales ingresados con exito'
+                   });
+           })
+           .catch((error:any) => {
+               res
+               .send({
+                   status: 'error',
+                   message: 'Lo datos no se pudieron ingresar, puede que no esten bien estructurados'
+               });
+           })
+    } else {
+        res
+           .send({
+               status: 'error',
+               message: 'Ocurrión un error tratando de insertar los datos personales'
+           });
+    }
+});
+
+/**
+ * Metodo para actualizar los datos personales de un paciente existente
+ */
+app.patch('/paciente/personal/:pacienteId', (req, res) => {
+   firebaseHelper.firestore
+       .checkDocumentExists(
+           db,
+           personalDataCollection,
+           req.params.pacienteId
+       )
+       .then((document:any) => {
+           if(document.exists) {
+               firebaseHelper.firestore
+                   .updateDocument(
+                       db,
+                       personalDataCollection,
+                       req.params.pacienteId,
+                       req.body
+                   )
+                   .then((success:any) => {
+                       res.send({
+                           status:'success',
+                           messsage: 'Se han actualizado los datos con exito'
+                       });
+                   })
+                   .catch((error:any) => {
+                       res.send({
+                           status: 'error', 
+                           message: 'Existió un error al intentar actualizar los datos'
+                       });
+                   });
+           } else {
+               res
+                   .status(404)
+                   .send({
+                       status: 'error',
+                       message: 'No existe el paciente'
+                   });
+           }
+       });
+});
+
+/**
+ * Metodo para conseguir los datos de un paciente determinado
+ */
+app.get('/paciente/personal/:pacienteId', (req, res) => {
+    firebaseHelper.firestore
+       .checkDocumentExists(
+           db, 
+           personalDataCollection,
+           req.params.pacienteId
+       )
+       .then((document:any) => {
+           if(document.exists) {
+               res
+               .status(200)
+               .send(document.data);
+           } else {
+               res
+                   .status(404)
+                   .send({
+                       status: 'error',
+                       message: 'El paciente aún no tiene datos personales'
+                   });
+           }
+       });
+});
+
+/**
+ * Metodo para eliminar los datos personales de un paciente existente
+ */
+app.delete('/paciente/personal/:pacienteId', (req, res) => {
+    firebaseHelper.firestore
+       .checkDocumentExists(
+           db,
+           personalDataCollection,
+           req.params.pacienteId
+       )
+       .then((document:any) =>{
+           if(document.exists) {
+               firebaseHelper.firestore
+                   .deleteDocument(
+                       db,
+                       personalDataCollection,
+                       req.params.pacienteId
+                   )
+                   .then((success:any) => {
+                       if(success.status) {
+                           res.send({
+                               status: 'success',
+                               message: 'Se han reseteado tus datos personales'
+                           });
+                       } else {
+                           res.send({
+                               status: 'error',
+                               message: 'Existió un error al intentar resetear tus datos personales'
+                           });
+                       }
+                   });
+           } else {
+               res
+                   .status(404)
+                   .send({
+                       status: 'error',
+                       message: 'No existe el paciente'
+                   });
+           }
+       });
+});
+
+
+
+// CRUD de la api para el ingreso de los datos del paciente
+
+/**
+ * ================ RUTAS PARA DATOS BASICOS DEL PACIENTE
+ */
+
+ /**
+  * Metodo para insertar nuevos datos de un paciente, como id del documento
+  * a insertar se ocupa el id del paciente.
+  */
+ app.post('/paciente/alergiasyantecedentes', (req, res) => {
+    const body = req.body;
+    if(body != null) {
+        firebaseHelper.firestore
+            .createDocumentWithID(
+                db,
+                pacientesAlAnDataCollection,
+                body.paciente_id,
+                {
+                    "alergias": body.alergias,
+                    "antecedentes": body.antecedentes
+                }
+            )
+            .then((response:any) => {
+                res
+                .status(201).send({
+                    status: "success",
+                    message: "Datos ingresados correctamente"
+                })
+            })
+            .catch((error:any) => {
+                res.send({
+                    status: 'error', 
+                    message: 'Datos mal estructurados'
+                });
+            });
+    } else {
+        res.send({
+            status: "error",
+            message: "Datos inexistentes"
+        });
+    }
+});
+
+/**
+ * Metodo para actualizar parcial o completamente los datos de un paciente
+ * dado su id
+ */
+app.patch("/paciente/alergiasyantecedentes/:pacienteId", (req, res) => {
+    firebaseHelper.firestore
+        .updateDocument(
+            db,
+            pacientesAlAnDataCollection,
+            req.params.pacienteId,
+            req.body
+        )
+        .then((success:any) =>{
+            res.send({
+                status: 'success',
+                message: 'Datos del paciente actualizados'
+            });
+        })
+        .catch((error:any) => {
+            res.send({
+                status: 'error', 
+                message: 'Existió un error al intentar actualizar los datos'
+            })
+        });
+});
+
+/**
+ * Metodo para obtener los datos de un paciente determinado en base a su id,
+ * y si es que este existe
+ */
+app.get("/paciente/alergiasyantecedentes/:pacienteId", (req, res) => {
+    firebaseHelper.firestore
+        .checkDocumentExists(
+            db,
+            pacientesAlAnDataCollection,
+            req.params.pacienteId
+        )
+        .then((document:any) => {
+            if(document.exists) {
+                res
+                    .status(200)
+                    .send(document.data);
+            } else {
+                res
+                    .status(404)
+                    .send({
+                        status: "error",
+                        message: "No existe el paciente"
+                    });
+            }
+        });
+});
+
+/**
+ * Metodo para eliminar los datos de alergias y antecedentes de un paciente
+ * determinado
+ */
+app.delete("/paciente/alergiasyantecedentes/:pacienteId", (req, res) => {
+    firebaseHelper.firestore
+        .checkDocumentExists(
+            db,
+            pacientesAlAnDataCollection,
+            req.params.pacienteId
+        )
+        .then((document:any) => {
+            if(document.exists){
+                firebaseHelper.firestore
+                    .deleteDocument(
+                        db,
+                        pacientesAlAnDataCollection,
+                        req.params.pacienteId
+                    )
+                    .then((success:any) => {
+                        if(success.status) {
+                            res.send({
+                                status: 'success',
+                                message: 'Tus datos de alergias y antecedentes han sido reseteados'
+                            });
+                        } else {
+                            res
+                                .send({
+                                    status: 'error',
+                                    message: 'Error al tratar de resetear tus datos de alergias y antecedentes'
+                                });
+                        }
+                    });
+            } else {
+                res
+                    .status(404)
+                    .send('No existe el paciente');
+            }
+        });
+});
+
+
+// inicio de CRUD y RUTAS PARA MOSTRAR LOS DATOS PUBLICOS DEL PACIENTE
+
+
+app.post('/paciente/public', (req, res) => {
+	const body = req.body;
+	if(body != null){
+	firebaseHelper.firestore
+		.createDocumentWithID(
+			db,
+			publicDataCollection,
+			body.paciente_id,
+			{
+				'nombre': body.nombre,
+				'email': body.email,
+				'telefono': body.telefono,
+				'tipo_sangre': body.tipo_sangre,
+				'pais': body.pais,
+				'alergias': body.alergias
+			}
+		)
+		.then((response:any) => {
+			res
+				.status(201)
+				.send({
+					status: 'success',
+					message: 'Se han configurado correctamente tus datos publicos'
+				});
+		})
+		.catch((error:any) => {
+			res
+				.send({
+					status: 'error',
+					'message': 'Tus datos publicos no pudieron ser configurados'
+				});
+		});
+	} else {
+		res
+			.send({
+				status: 'error',
+				message: 'No existen datos para poner publico'
+			});
+	}
+});
+
